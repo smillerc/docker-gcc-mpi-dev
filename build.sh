5bbf27254d6844fe7e6fc7ca2bb41f8e7ae0ba32
Miller, Samuel <smil@lle.rochester.edu>
2019-08-02 20:27:18 -0400

    build script fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 tag=9.1
-docker build --build-arg -t smillerc/gcc-mpi-dev:${tag} -f Dockerfile .
+docker build -t smillerc/gcc-mpi-dev:${tag} -f Dockerfile .
 docker push smillerc/gcc-mpi-dev:${tag}
